fd81364319c66000084468c818cd266bf0adadec
danilodevapp <danilodevapp@gmail.com>
2024-05-08 16:08:01 -0300

    Acrescentado o SwuitchCase

---
+++ controle-de-fluxo-java/src/switchCase.java
@@ -0,0 +1,27 @@
+public class switchCase {
+    public static void main(String[] args) {
+        String sigla = "L";
+
+        switch (sigla) {
+            case "P":{ 
+                System.out.println("Pequeno");
+            }
+            break;
+
+            case "M": {
+                System.out.println("Médio");
+            }
+            break;
+
+            case "G": {
+                System.out.println("Grande");
+            }
+            break;
+
+            default:
+                System.out.println("Indefinido");                
+            break;
+        }
+    }
+
+}
